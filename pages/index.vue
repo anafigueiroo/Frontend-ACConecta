be8315b0549ca3681fc28a82c4fe96bd1ad90f5a
Ana Vitoria <ana.vf16@aluno.ifsc.edu.br>
2025-03-04 14:06:09 -0300

    Cria home e tela de login

---
+++ pages/index.vue
@@ -0,0 +1,25 @@
+<template>
+  <NuxtRouteAnnouncer />
+
+  <div class="flex flex-col items-center justify-center min-h-screen p-5 bg-white">
+    <div>
+      <img src="assets/images/logo-acc-conecta.png" alt="ACC Conecta" class="w-60">
+    </div>
+
+    <h1 class="text-center text-lg font-semibold text-gray-700">
+      Descubra atividades para suas horas complementares!
+    </h1>
+    <p class="text-center text-gray-500 text-sm mt-2">
+      Faça login e encontre oportunidades para se destacar na Unochapecó!
+    </p>
+
+    <div class="w-full max-w-xs mt-5 mx-0 flex flex-col items-center">
+      <BaseLinkButton href="/login" primary> Entrar</BaseLinkButton>
+      <BaseLinkButton href="/register"> Cadastre-se</BaseLinkButton>
+    </div>
+
+    <a href="/login" class="mt-4 text-sm text-gray-500 hover:underline">
+      Esqueceu a sua senha?
+    </a>
+  </div>
+</template>
---
+++ pages/login.vue
@@ -0,0 +1,52 @@
+<template>
+  <div class="flex flex-col items-center justify-center min-h-screen p-5 bg-white">
+    <div>
+      <img src="assets/images/logo-acc-conecta.png" alt="ACC Conecta" class="w-60">
+    </div>
+
+    <div class="w-full max-w-xs mx-0 flex flex-col items-center">
+      <BaseInput
+        label="Email"
+        placeholder="Digite seu email"
+        type="email"
+        name="email"
+        id="email"
+        :validationRules="[
+          emailValidation
+        ]"
+      />
+
+      <BaseInput
+        label="Nome"
+        placeholder="Digite seu nome"
+        name="name"
+        id="name"
+        :validationRules="[
+          nameValidation
+        ]"
+      />
+
+      <BaseLinkButton href="/login" primary class="mt-8"> Entrar</BaseLinkButton>
+    </div>
+
+    <a href="/register" class="mt-4 text-sm text-gray-500 hover:underline">
+      Não tem uma conta? Cadastre-se
+    </a>
+  </div>
+</template>
+
+<script setup>
+  const emailValidation = (value) => {
+    if (!value.includes('@')) {
+      return { valid: false, message: 'Por favor, insira um email válido.' };
+    }
+    return { valid: true, message: '' };
+  };
+
+  const nameValidation = (value) => {
+    if (value.length < 8) {
+      return { valid: false, message: 'O nome deve ter pelo menos 8 caracteres.' };
+    }
+    return { valid: true, message: '' };
+  };
+</script>
