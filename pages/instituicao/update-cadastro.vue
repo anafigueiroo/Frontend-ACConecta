da9d8fb93326d014a91d92a4e62e6623b95c3f37
Ana Vitória Figueiró <ana16vivi@gmail.com>
2025-06-19 22:39:22 -0300

    Adiciona tela de usuário e cadastro de instituicao

---
+++ pages/instituicao/update-cadastro.vue
@@ -0,0 +1,65 @@
+<template>
+  <div class="bg-white flex items-center justify-center px-4 py-10">
+    <div class="w-full max-w-md text-center">
+      <h2 class="text-xl font-semibold text-gray-800 mb-6">Alterar cadastro</h2>
+
+      <form @submit.prevent="alterarDados" class="space-y-5 text-left">
+
+        <div>
+          <label for="nome" class="block text-sm font-medium text-gray-900 mb-1">Nome</label>
+          <div class="flex items-center rounded-md bg-white pl-3 outline outline-1 outline-gray-300 focus-within:outline-2 focus-within:outline-blue-400">
+            <input type="text" v-model="nome" id="nome" placeholder="Informe seu nome"
+              class="w-full py-3.5 pr-3 pl-1 text-base text-gray-900 placeholder:text-gray-400 focus:outline-none rounded-md" />
+          </div>
+        </div>
+
+        <div>
+          <label for="cpf" class="block text-sm font-medium text-gray-900 mb-1">CNPJ</label>
+          <div class="flex items-center rounded-md bg-white pl-3 outline outline-1 outline-gray-300 focus-within:outline-2 focus-within:outline-blue-400">
+            <input type="text" id="cpf" placeholder="Informe seu CPF"
+              class="w-full py-3.5 pr-3 pl-1 text-base text-gray-900 placeholder:text-gray-400 focus:outline-none rounded-md" />
+          </div>
+        </div>
+
+        <div>
+          <label for="email" class="block text-sm font-medium text-gray-900 mb-1">E-mail</label>
+          <div class="flex items-center rounded-md bg-white pl-3 outline outline-1 outline-gray-300 focus-within:outline-2 focus-within:outline-blue-400">
+            <input type="email" id="email" placeholder="Informe seu e-mail"
+              class="w-full py-3.5 pr-3 pl-1 text-base text-gray-900 placeholder:text-gray-400 focus:outline-none rounded-md" />
+          </div>
+        </div>
+
+        <div>
+          <label for="telefone" class="block text-sm font-medium text-gray-900 mb-1">Telefone</label>
+          <div class="flex items-center rounded-md bg-white pl-3 outline outline-1 outline-gray-300 focus-within:outline-2 focus-within:outline-blue-400">
+            <input type="text" id="telefone" placeholder="Informe seu telefone"
+              class="w-full py-3.5 pr-3 pl-1 text-base text-gray-900 placeholder:text-gray-400 focus:outline-none rounded-md" />
+          </div>
+        </div>
+
+        <div class="pt-4 space-y-3">
+          <button type="submit"
+            class="w-full bg-blue-500 hover:bg-blue-600 text-white font-semibold py-3 rounded-full transition">
+            Atualizar
+          </button>
+          <button type="submit"
+            class="w-full border border-blue-400 text-blue-500 font-semibold py-3 rounded-full hover:bg-blue-50 transition">
+            Cancelar
+          </button>
+        </div>
+      </form>
+    </div>
+  </div>
+</template>
+
+<script setup>
+import { ref } from 'vue'
+
+const nome = ref('')
+
+const router = useRouter()
+
+function alterarDados() {
+      router.push('/instituicao/user')
+}
+</script>
---
+++ pages/instituicao/user.vue
@@ -0,0 +1,49 @@
+<template>
+  <div class="flex flex-col items-center px-4 py-6 max-w-md mx-auto sm:max-w-lg md:max-w-2xl">
+    <div class="w-32 h-32 rounded-full bg-gray-100 flex items-center justify-center mb-4">
+      <IconUser class="w-10 h-10 text-gray-500" />
+    </div>
+
+    <div class="text-center mb-6">
+      <h2 class="text-lg font-semibold text-gray-800">Instituição</h2>
+      <p class="text-sm text-gray-600 underline">empresa@gmail.com</p>
+    </div>
+
+    <div class="bg-gray-100 text-sm text-gray-700 p-4 rounded-md w-full mb-6">
+      <p class="mb-2"><strong>CNPJ:</strong> xxx.xxx.xxx-xx</p>
+      <p class="mb-2"><strong>Telefone:</strong> (49) 98899-9988</p>
+    </div>
+
+    <div class="w-full space-y-4 mb-4">
+      <NuxtLink to="update-cadastro" class="justify-self-center">
+        <button class="grid grid-cols-[1fr_auto] items-center w-full bg-blue-400 text-white py-4 px-4 rounded-full hover:bg-blue-500 mt-4">
+          <span class="justify-self-center">Alterar cadastro</span>
+          <ArrowRight class="w-5 h-5" />
+        </button>
+      </NuxtLink>
+      <button class="w-full border-2 border-blue-400 text-blue-600 py-4 rounded-full font-semibold hover:bg-blue-50 mt-4">
+        Alterar senha
+      </button>
+    </div>
+
+    <div class="flex justify-between items-center w-full text-sm text-gray-600 mt-4">
+      <NuxtLink to="/" class="underline hover:text-red-600">Excluir conta</NuxtLink>
+      <button class="flex items-center gap-1 hover:text-red-600" @click="sair">
+        <LogOut class="w-4 h-4" />
+        Logout
+      </button>
+    </div>
+  </div>
+</template>
+
+<script setup>
+    import { User as IconUser, ArrowRight, LogOut } from 'lucide-vue-next'
+
+    const router = useRouter()
+    const { logout } = useAuth()
+
+    function sair() {
+    logout()
+    router.push('/')
+    }
+</script>
